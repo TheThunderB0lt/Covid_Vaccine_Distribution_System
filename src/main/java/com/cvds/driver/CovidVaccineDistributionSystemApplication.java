ec5fd0bed3817ddbd4e7af52fbcf89d4e231bc50
TheThunderB0lt <nikithkumarniki160698@gmail.com>
2023-12-21 11:22:07 +0530

    initial Commit

---
+++ src/main/java/com/cvds/driver/CovidVaccineDistributionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cvds.driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidVaccineDistributionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidVaccineDistributionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvds/driver/CovidVaccineDistributionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvds.driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidVaccineDistributionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
